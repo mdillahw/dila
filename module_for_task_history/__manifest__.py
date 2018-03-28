8d2851b50fe04a5a88493730277858cf81eb43a7
abdillahwahab <mdillahw@nurosoft-indonesia.com>
2018-03-28 10:57:29 +0700

    Update for commit

--- module_for_task_history/__manifest__.py
+++ module_for_task_history/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Task History of Version",
     "author": "abdillahwahab",
     "website": "www.abdillahwahab.com",
-    "version": "1.3",
+    "version": "1.4",
     "category": "project",
     "depends": [
         "base",
